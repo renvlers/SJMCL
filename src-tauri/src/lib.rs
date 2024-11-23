1609d2adba88d23b14674e717a1d44100de50f5f
UNIkeEN <94227543+UNIkeEN@users.noreply.github.com>
2024-11-24 01:03:29 +0800

    feat: add root font css, hide menu bar on macOS

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,12 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
+use tauri::menu::MenuBuilder;
+
 pub fn run() {
   tauri::Builder::default()
     .setup(|app| {
+      let menu = MenuBuilder::new(app).build()?;
+      app.set_menu(menu)?;
+
       if cfg!(debug_assertions) {
         app.handle().plugin(
           tauri_plugin_log::Builder::default()
