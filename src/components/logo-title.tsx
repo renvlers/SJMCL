9a0713377b05f6e2a353565d6e5f668bb26e8a00
UNIkeEN <94227543+UNIkeEN@users.noreply.github.com>
2024-11-26 14:53:12 +0800

    feat(frontend): add independent logo and title component

---
+++ src/components/logo-title.tsx
@@ -0,0 +1,30 @@
+import {
+  Heading,
+  Highlight,
+  BoxProps
+} from '@chakra-ui/react';
+
+import styles from './logo-title.module.css';
+
+interface LogoTitleProps extends BoxProps {}
+
+export const TitleShort: React.FC<LogoTitleProps> = (props) => {
+  return (
+    <Heading size="md" className={styles.title + " no-select"} {...props}>
+      <Highlight query="L" styles={{ color: "blue.600" }}>
+        SJMCL
+      </Highlight>
+    </Heading>
+  );
+}
+
+export const TitleFull: React.FC<LogoTitleProps> = (props) => {
+  return (
+    <Heading size="md" className={styles.title + " no-select"} {...props}>
+      <Highlight query="L" styles={{ color: "blue.600" }}>
+        SJMC Launcher
+      </Highlight>
+    </Heading>
+  );
+}
+
