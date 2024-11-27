52714d1c241ffb73794cc1ee11302d85fa1de600
UNIkeEN <94227543+UNIkeEN@users.noreply.github.com>
2024-11-27 21:12:43 +0800

    feat(frontend): initially add launch page, only UI design

--- src/pages/launch.tsx
+++ src/pages/launch.tsx
@@ -1,5 +1,101 @@
+import { 
+  Button,
+  Tooltip,
+  VStack,
+  HStack,
+  Image,
+  Text,
+  Card,
+  Box,
+  IconButton,
+  IconButtonProps
+} from "@chakra-ui/react";
+import { LuArrowLeftRight } from "react-icons/lu";
+import { useTranslation } from "react-i18next";
+import { useRouter } from "next/router";
+import { mockRoleList } from "@/models/account";
+import styles from "./launch.module.css";
+
+interface SwitchButtonProps extends IconButtonProps {
+  tooltip: string;
+}
+
+const SwitchButton: React.FC<SwitchButtonProps> = ({ tooltip, ...props}) => {
+  return (
+    <Tooltip label={tooltip} placement="top-end" fontSize="sm">
+      <IconButton 
+        size="xs"
+        position="absolute" top={2} right={2}
+        icon={<LuArrowLeftRight />}
+        {...props}
+      />
+    </Tooltip>
+  )
+}
+
 const LaunchPage = () => {
-  return <div>Launch Page</div>
+  const { t } = useTranslation();
+  const router = useRouter();
+
+  const role = mockRoleList[0]; // only for mock
+
+  return (
+    <HStack position="absolute" bottom={7} right={7} spacing={4}>
+      <Card className={styles["selected-user-card"]}>
+          <SwitchButton 
+            tooltip={t("LaunchPage.SwitchButton.tooltip.switchRole")}
+            aria-label="switch-role" 
+            onClick={() => router.push("/accounts")}
+          />
+        <HStack spacing={2.5} h="100%" w="100%">
+          <Image
+            boxSize="32px"
+            objectFit="cover"
+            src={role.avatarUrl}
+            alt={role.name}
+          />
+          <VStack spacing={0} align="left" mt={-2}>
+            <Text 
+              fontSize="sm" 
+              className="no-select"
+              fontWeight="bold"
+              mt={2}
+            >
+              {role.name}
+            </Text>
+            <Text fontSize="2xs" className="secondary-text no-select">
+              {t(`Enums.roleTypes.${role.type}`)}
+            </Text>
+            <Text fontSize="2xs" className="secondary-text no-select">
+              {role.type === "3rdparty" && role.authServer?.name}
+            </Text>
+          </VStack>
+        </HStack>
+      </Card>
+      <Box position="relative">
+        <Button
+          colorScheme="blackAlpha"
+          className={styles["launch-button"]}
+        >
+          <VStack spacing={1.5}>
+            <Text fontSize="lg" fontWeight="bold">启动游戏</Text>
+            <Text fontSize="sm">尚未选择游戏实例</Text>
+          </VStack>
+        </Button>
+
+        <SwitchButton 
+          tooltip={t("LaunchPage.SwitchButton.tooltip.switchGame")}
+          aria-label="switch-game"
+          colorScheme="blackAlpha"
+          onClick={() => router.push("/games")}
+          position="absolute"
+          top={1}
+          right={1}
+        />
+      </Box>
+    </HStack>
+    
+  )
 };
 
 export default LaunchPage;
