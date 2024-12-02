7a1904fd74d76a9df079b40dbb73153ae26f7260
w1049 <w10493wang@163.com>
2024-12-02 22:51:26 +0800

    feat(backend): add launcher config manager

---
+++ src-tauri/src/launcher_config.rs
@@ -0,0 +1,132 @@
+use serde::{Deserialize, Serialize};
+use std::sync::LazyLock;
+use std::{path::PathBuf, sync::Mutex};
+use tauri::State;
+
+#[tauri::command]
+pub fn get_launcher_config(state: State<'_, Mutex<LauncherConfig>>) -> LauncherConfig {
+  state.lock().unwrap().clone()
+}
+
+#[tauri::command]
+pub fn update_launcher_config(
+  launcher_config: LauncherConfig,
+  state: State<'_, Mutex<LauncherConfig>>,
+) {
+  let mut state = state.lock().unwrap();
+  *state = launcher_config;
+  save_config(&state);
+  // match &key_path as &str {
+  //   "version" => state.version = value,
+  //   "mocked" => state.mocked = value == "true",
+  //   "appearance.theme.primarColor" => state.appearance.theme.primary_color = value,
+  //   "appearance.background.presetChoice" => state.appearance.background.preset_choice = value,
+  //   "download.source.strategy" => state.download.source.strategy = value,
+  //   "download.download.autoConcurrent" => state.download.download.auto_concurrent = value == "true",
+  //   "download.download.concurrentCount" => {
+  //     state.download.download.concurrent_count = value.parse::<u32>().ok()
+  //   }
+  //   "download.download.enableSpeedLimit" => {
+  //     state.download.download.enable_speed_limit = value == "true"
+  //   }
+  //   "download.download.speedLimitValue" => {
+  //     state.download.download.speed_limit_value = value.parse::<u32>().ok()
+  //   }
+  //   "download.cache.directory" => state.download.cache.directory = value,
+  //   "general.general.language" => state.general.general.language = value,
+  //   _ => (),
+  // };
+}
+
+static CONFIG_PATH: LazyLock<PathBuf> = LazyLock::new(|| {
+  std::env::current_exe()
+    .unwrap()
+    .parent()
+    .unwrap()
+    .join("launcher_config.json")
+});
+
+pub fn read_or_default() -> LauncherConfig {
+  if let Ok(config) = std::fs::read_to_string(CONFIG_PATH.as_path()) {
+    serde_json::from_str(&config).unwrap_or_default()
+  } else {
+    LauncherConfig::default()
+  }
+}
+
+pub fn save_config(config: &LauncherConfig) {
+  let config = serde_json::to_string_pretty(config).unwrap();
+  std::fs::write(CONFIG_PATH.as_path(), config).unwrap();
+}
+
+structstruck::strike! {
+  #[strikethrough[derive(Debug, PartialEq, Eq, Clone, Deserialize, Serialize)]]
+  #[strikethrough[serde(rename_all = "camelCase", deny_unknown_fields)]]
+  pub struct LauncherConfig {
+    pub version: String,
+    pub mocked: bool,
+    pub appearance: struct {
+      pub theme: struct {
+        pub primary_color: String,
+      },
+      pub background: struct {
+        pub preset_choice: String,
+      }
+    },
+    pub download: struct DownloadConfig {
+      pub source: struct {
+        pub strategy: String,
+      },
+      pub download: struct {
+        pub auto_concurrent: bool,
+        pub concurrent_count: Option<usize>,
+        pub enable_speed_limit: bool,
+        pub speed_limit_value: Option<usize>,
+      },
+      pub cache: struct {
+        pub directory: String,
+      }
+    },
+    pub general: struct GeneralConfig {
+      pub general: struct {
+        pub language: String,
+      }
+    }
+  }
+}
+
+impl Default for LauncherConfig {
+  fn default() -> Self {
+    Self {
+      version: "dev".to_string(),
+      mocked: false,
+      appearance: Appearance {
+        theme: Theme {
+          primary_color: "blue".to_string(),
+        },
+        background: Background {
+          preset_choice: "Jokull".to_string(),
+        },
+      },
+      download: DownloadConfig {
+        source: Source {
+          strategy: "auto".to_string(),
+        },
+        download: Download {
+          auto_concurrent: true,
+          concurrent_count: None,
+          enable_speed_limit: false,
+          speed_limit_value: None,
+        },
+        cache: Cache {
+          directory: "/mock/path/to/cache/".to_string(),
+        },
+      },
+      general: GeneralConfig {
+        general: General {
+          language: "zh-Hans".to_string(),
+        },
+      },
+    }
+  }
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,25 @@
+use std::sync::Mutex;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 use tauri::menu::MenuBuilder;
+use tauri::Manager;
+
+mod launcher_config;
 
 pub fn run() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![
+      launcher_config::get_launcher_config,
+      launcher_config::update_launcher_config
+    ])
     .plugin(tauri_plugin_shell::init())
     .setup(|app| {
+      // Set the launcher config
+      let mut launcher_config = launcher_config::read_or_default();
+      launcher_config.version = app.package_info().version.to_string();
+      launcher_config::save_config(&launcher_config);
+      app.manage(Mutex::new(launcher_config));
+
       let menu = MenuBuilder::new(app).build()?;
       app.set_menu(menu)?;
 
