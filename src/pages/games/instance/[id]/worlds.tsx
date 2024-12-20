e561d4f2a1a74dde19bae9f2a75b5c823043637b
HsxMark <hsxmark@outlook.com>
2024-12-20 16:43:44 +0800

    feat(frontend/page): add resourcepacks page and fix error in worlds page

--- src/pages/games/instance/[id]/worlds.tsx
+++ src/pages/games/instance/[id]/worlds.tsx
@@ -1,10 +1,10 @@
-import { IconButton, Image, Text, VStack } from "@chakra-ui/react";
+import { IconButton, Image, Text, Tooltip, VStack } from "@chakra-ui/react";
 import { open } from "@tauri-apps/plugin-shell";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { LuFolderOpen } from "react-icons/lu";
 import Empty from "@/components/common/empty";
-import { OptionItem } from "@/components/common/option-item";
+import { OptionItem, OptionItemGroup } from "@/components/common/option-item";
 import { GameServerInfo, WorldInfo } from "@/models/game-instance";
 import { mockGameserver, mockWorlds } from "@/models/mock/game-instance";
 import { formatRelativeTime } from "@/utils/datetime";
@@ -20,64 +20,70 @@ const InstanceWorldsPage = () => {
   }, []);
 
   return (
-    <VStack spacing={6} align="stretch">
-      <Text fontWeight="bold" fontSize="md">
+    <VStack spacing={2.5} align="stretch">
+      <Text fontWeight="bold" fontSize="sm">
         {t("InstanceWorldsPage.worldList.title")}
       </Text>
       {worlds.length > 0 ? (
         <VStack spacing={4} align="stretch">
-          {worlds.map((world) => (
-            <OptionItem
-              key={world.name}
-              title={world.name}
-              description={`${t("InstanceWorldsPage.worldList.lastPlayedAt")} ${formatRelativeTime(world.lastPlayedAt, t)}`}
-              prefixElement={
-                <Image
-                  src={world.iconUrl}
-                  alt={world.name}
-                  width={30}
-                  height={30}
-                  style={{ borderRadius: "4px" }}
-                />
-              }
-            >
-              <IconButton
-                aria-label={"open"}
-                icon={<LuFolderOpen />}
-                variant="ghost"
-                size="sm"
-                onClick={() => open(world.fileDir)}
-              />
-            </OptionItem>
-          ))}
+          <OptionItemGroup
+            items={worlds.map((world) => (
+              <OptionItem
+                key={world.name}
+                title={world.name}
+                description={`${t("InstanceWorldsPage.worldList.lastPlayedAt")} ${formatRelativeTime(world.lastPlayedAt, t)}`}
+                prefixElement={
+                  <Image
+                    src={world.iconUrl}
+                    alt={world.name}
+                    width={30}
+                    height={30}
+                    style={{ borderRadius: "4px" }}
+                  />
+                }
+              >
+                <Tooltip label={t("General.openFolder")}>
+                  <IconButton
+                    aria-label={"open"}
+                    icon={<LuFolderOpen />}
+                    variant="ghost"
+                    size="sm"
+                    onClick={() => open(world.fileDir)}
+                  />
+                </Tooltip>
+              </OptionItem>
+            ))}
+          />
         </VStack>
       ) : (
         <Empty withIcon={false} size="sm" />
       )}
 
-      <Text fontWeight="bold" fontSize="md">
+      <Text fontWeight="bold" fontSize="sm">
         {t("InstanceWorldsPage.serverList.title")}
       </Text>
       {gameServers.length > 0 ? (
-        <VStack spacing={4} align="stretch">
-          {gameServers.map((server) => (
-            <OptionItem
-              key={server.name}
-              title={server.name}
-              description={server.ip}
-              prefixElement={
-                <Image
-                  src={server.icon}
-                  alt={server.name}
-                  width={30}
-                  height={30}
-                  style={{ borderRadius: "4px" }}
-                />
-              }
-            >
-              <></>
-            </OptionItem>
-          ))}
+        <VStack spacing={2.5} align="stretch">
+          <OptionItemGroup
+            items={gameServers.map((server) => (
+              <OptionItem
+                key={server.name}
+                title={server.name}
+                description={server.ip}
+                prefixElement={
+                  <Image
+                    src={server.icon}
+                    alt={server.name}
+                    width={30}
+                    height={30}
+                    style={{ borderRadius: "4px" }}
+                  />
+                }
+              >
+                <></>
+              </OptionItem>
+            ))}
+          />
         </VStack>
       ) : (
         <Empty withIcon={false} size="sm" />
