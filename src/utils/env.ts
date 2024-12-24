98e5eb457ec70640fc5d10f1dc1218f6038d3657
UNIkeEN <94227543+UNIkeEN@users.noreply.github.com>
2024-12-24 23:47:35 +0800

    feat(frontend): add frontend compatibility check function initially

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,3 +1,23 @@
 export const isDev = process.env.NODE_ENV === "development";
 
 export const isProd = process.env.NODE_ENV === "production";
+
+export const checkFrontendCompatibility = () => {
+  const checkList = [
+    {
+      label: "backdrop-filter-blur",
+      check: () =>
+        CSS.supports("backdrop-filter", "blur(1px)") ||
+        CSS.supports("-webkit-backdrop-filter", "blur(1px)"),
+      // TODO: on ubuntu, the check may be passed but still not display blur on bg correctly.
+    },
+  ];
+
+  const failedChecks = checkList
+    .filter((capability) => !capability.check())
+    .map((capability) => capability.label);
+
+  const allPassed = failedChecks.length === 0;
+
+  return { allPassed, failedChecks };
+};
