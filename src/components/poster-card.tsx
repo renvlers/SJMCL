4f29cda6a681c99050e35f5b40c20167480d6952
UNIkeEN <94227543+UNIkeEN@users.noreply.github.com>
2024-12-27 22:37:27 +0800

    refactor(frontend): show border when hover on poster card

--- src/components/poster-card.tsx
+++ src/components/poster-card.tsx
@@ -7,6 +7,7 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { open } from "@tauri-apps/plugin-shell";
+import { useState } from "react";
 import { useLauncherConfig } from "@/contexts/config";
 import { PostSummary } from "@/models/post";
 
@@ -17,14 +18,21 @@ interface PosterCardProps extends CardProps {
 // used in discover page, under masonry container
 const PosterCard = ({ data }: PosterCardProps) => {
   const { config } = useLauncherConfig();
+  const primaryColor = config.appearance.theme.primaryColor;
+
   const { title, abstract, imageSrc, link } = data;
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <Card
       cursor="pointer"
       overflow="hidden" // show the border
       p={0}
+      borderColor={`${primaryColor}.500`}
+      variant={isHovered ? "outline" : "elevated"}
       onClick={() => open(link)}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
       {imageSrc && <Image objectFit="cover" src={imageSrc} alt={title} />}
       <CardBody className="content-card">
