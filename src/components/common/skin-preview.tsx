fcab2cca0b4c584fea88bf106e348f458fc196c5
UNIkeEN <94227543+UNIkeEN@users.noreply.github.com>
2025-01-17 16:12:33 +0800

    feat(frontend): add background selector in skin-preview component
    Co-authored-by: Kilox <120613056+KiloxGo@users.noreply.github.com>
    Co-authored-by: baiyuansjtu <sun060319@sjtu.edu.cn>

--- src/components/common/skin-preview.tsx
+++ src/components/common/skin-preview.tsx
@@ -4,6 +4,10 @@ import {
   Flex,
   HStack,
   IconButton,
+  Popover,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
   Switch,
   Text,
   Tooltip,
@@ -11,11 +15,24 @@ import {
 import { useEffect, useMemo, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { BsPersonRaisedHand } from "react-icons/bs";
-import { FaPerson, FaPersonRunning, FaPersonWalking } from "react-icons/fa6";
-import { LuPause, LuPlay, LuRefreshCw, LuRefreshCwOff } from "react-icons/lu";
+import {
+  FaCircleCheck,
+  FaPerson,
+  FaPersonRunning,
+  FaPersonWalking,
+  FaRegCircle,
+} from "react-icons/fa6";
+import {
+  LuChevronUp,
+  LuPause,
+  LuPlay,
+  LuRefreshCw,
+  LuRefreshCwOff,
+} from "react-icons/lu";
 import * as skinview3d from "skinview3d";
 
 type AnimationType = "idle" | "walk" | "run" | "wave";
+type backgroundType = "none" | "black" | "panorama";
 
 interface SkinPreviewProps extends Omit<BoxProps, "width" | "height"> {
   skinSrc?: string;
@@ -23,6 +40,7 @@ interface SkinPreviewProps extends Omit<BoxProps, "width" | "height"> {
   width?: number;
   height?: number;
   animation?: AnimationType;
+  canvasBg?: backgroundType;
   showCape?: boolean;
   showControlBar?: boolean;
 }
@@ -33,6 +51,7 @@ const SkinPreview: React.FC<SkinPreviewProps> = ({
   width = 300,
   height = 400,
   animation = "walk",
+  canvasBg = "none",
   showCape = true,
   showControlBar = true,
   ...props
@@ -44,10 +63,27 @@ const SkinPreview: React.FC<SkinPreviewProps> = ({
   );
   const [currentAnimation, setCurrentAnimation] =
     useState<AnimationType>(animation);
+  const [background, setBackground] = useState<backgroundType>(canvasBg);
   const [autoRotate, setAutoRotate] = useState(false);
   const [isPlaying, setIsPlaying] = useState(true);
   const [isCapeVisible, setIsCapeVisible] = useState(showCape);
 
+  useEffect(() => {
+    setIsCapeVisible(showCape);
+  }, [showCape]);
+
+  useEffect(() => {
+    if (skinViewer) {
+      skinViewer.loadSkin(skinSrc);
+      if (isCapeVisible && capeSrc) {
+        skinViewer.loadCape(capeSrc);
+      } else {
+        skinViewer.loadCape(null);
+      }
+    }
+  }, [skinViewer, skinSrc, capeSrc, isCapeVisible]);
+
+  // animation
   const animationList = useMemo(
     () => ({
       idle: { icon: <FaPerson />, animation: new skinview3d.IdleAnimation() },
@@ -86,21 +122,6 @@ const SkinPreview: React.FC<SkinPreviewProps> = ({
   }, [width, height, showControlBar, skinViewer, animationList.walk.animation]);
 
   useEffect(() => {
-    setIsCapeVisible(showCape);
-  }, [showCape]);
-
-  useEffect(() => {
-    if (skinViewer) {
-      skinViewer.loadSkin(skinSrc);
-      if (isCapeVisible && capeSrc) {
-        skinViewer.loadCape(capeSrc);
-      } else {
-        skinViewer.loadCape(null);
-      }
-    }
-  }, [skinViewer, skinSrc, capeSrc, isCapeVisible]);
-
-  useEffect(() => {
     if (skinViewer) {
       skinViewer.autoRotate = isPlaying && autoRotate;
       if (isPlaying) {
@@ -112,12 +133,105 @@ const SkinPreview: React.FC<SkinPreviewProps> = ({
     }
   }, [skinViewer, autoRotate, currentAnimation, isPlaying, animationList]);
 
+  // background
+  const backgroundList = useMemo(
+    () => ({
+      none: {
+        colorScheme: "black",
+        btnVariant: "outline",
+        operation: () => {
+          if (skinViewer) skinViewer.background = null;
+        },
+      },
+      black: {
+        colorScheme: "gray",
+        btnVariant: "solid",
+        operation: () => {
+          if (skinViewer) skinViewer.background = "#2D3748";
+        },
+      },
+      panorama: {
+        bg: "/images/skins/panorama.jpg",
+        colorScheme: "blackAlpha",
+        btnVariant: "solid",
+        operation: () => {
+          if (skinViewer) skinViewer.loadPanorama("/images/skins/panorama.jpg");
+        },
+      },
+    }),
+    [skinViewer]
+  );
+
+  const backgroundTypes = Object.keys(backgroundList) as backgroundType[];
+
+  const BackGroundSelector = () => {
+    return (
+      <Popover placement="top-start">
+        <PopoverTrigger>
+          <IconButton
+            size="xs"
+            colorScheme={backgroundList[background].colorScheme}
+            variant={backgroundList[background].btnVariant}
+            mr={1}
+            aria-label="color"
+            icon={<LuChevronUp />}
+            style={
+              background === "panorama"
+                ? {
+                    backgroundImage: `url(${backgroundList["panorama"].bg})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                  }
+                : {}
+            }
+          />
+        </PopoverTrigger>
+        <PopoverContent width="auto" maxWidth="none">
+          <PopoverBody>
+            <HStack>
+              {backgroundTypes.map((type) => (
+                <IconButton
+                  key={type}
+                  size="xs"
+                  colorScheme={backgroundList[type].colorScheme}
+                  variant={backgroundList[type].btnVariant}
+                  aria-label="color"
+                  icon={
+                    type === background ? <FaCircleCheck /> : <FaRegCircle />
+                  }
+                  style={
+                    type === "panorama"
+                      ? {
+                          backgroundImage: `url(${backgroundList["panorama"].bg})`,
+                          backgroundSize: "cover",
+                          backgroundPosition: "center",
+                        }
+                      : {}
+                  }
+                  onClick={() => {
+                    setBackground(type);
+                    backgroundList[type].operation();
+                  }}
+                />
+              ))}
+            </HStack>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
+    );
+  };
+
+  useEffect(() => {
+    backgroundList[background].operation();
+  }, [background, backgroundList]);
+
   return (
     <Box {...props}>
       <canvas ref={canvasRef} />
       {showControlBar && (
         <Flex alignItems="center" justifyContent="space-between" mt={2}>
           <HStack spacing={0}>
+            <BackGroundSelector />
             <Tooltip label={t(`SkinPreview.animation.${currentAnimation}`)}>
               <IconButton
                 aria-label="Switch Animation"
