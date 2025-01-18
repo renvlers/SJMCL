639328ae017d3f88b16bfe8c48155674432d3b33
Qihao Sun <sun060319@sjtu.edu.cn>
2025-01-18 16:49:10 +0800

    feat(components): migrate and optimize the editable component (#58)
    * feat(components): migrate and optimize the editable component, resolve TS syntax issues and replace icons
    
    * refactor(components): refine Editable component by ensuring only one of Input and Textarea appears and simplifying props and styles
    
    * refactor(frontend): update Editable component to separate textProps and inputProps for Text/Input/Textarea control
    
    * fix(frontend): add full width to Text component in HStack

---
+++ src/components/common/editable.tsx
@@ -0,0 +1,180 @@
+import {
+  Box,
+  BoxProps,
+  FormControl,
+  FormErrorMessage,
+  HStack,
+  IconButton,
+  Input,
+  InputProps,
+  Text,
+  TextProps,
+  Textarea,
+  TextareaProps,
+} from "@chakra-ui/react";
+import { useEffect, useRef, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { LuCircleCheck, LuCircleX, LuPen } from "react-icons/lu";
+
+interface EditableProps extends BoxProps {
+  isTextArea: boolean;
+  value: string;
+  onEditSubmit: (value: string) => void;
+  localeKey?: string;
+  placeholder?: string;
+  checkError?: (value: string) => number;
+  onFocus?: () => void;
+  onBlur?: () => void;
+  textProps?: TextProps;
+  inputProps?: InputProps | TextareaProps;
+}
+
+const Editable: React.FC<EditableProps> = ({
+  isTextArea,
+  value,
+  onEditSubmit,
+  localeKey,
+  placeholder = "",
+  checkError = () => 0,
+  onFocus = () => {},
+  onBlur = () => {},
+  textProps = {},
+  inputProps = {},
+  ...boxProps
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [isInvalid, setIsInvalid] = useState(true);
+  const [tempValue, setTempValue] = useState(value);
+
+  const ref = useRef<HTMLElement | HTMLInputElement | HTMLTextAreaElement>(
+    null
+  );
+  const { t } = useTranslation();
+
+  const EditButtons = () => {
+    return isEditing ? (
+      <HStack ml="auto">
+        <IconButton
+          icon={<LuCircleCheck />}
+          size="sm"
+          aria-label="submit"
+          onClick={() => {
+            if (isInvalid) return;
+            if (tempValue !== value) onEditSubmit(tempValue);
+            setIsEditing(false);
+          }}
+        />
+        <IconButton
+          icon={<LuCircleX />}
+          size="sm"
+          aria-label="cancel"
+          onClick={() => {
+            setTempValue(value);
+            setIsEditing(false);
+            setIsInvalid(false);
+          }}
+        />
+      </HStack>
+    ) : (
+      <IconButton
+        icon={<LuPen />}
+        size="sm"
+        aria-label="edit"
+        onClick={() => {
+          setTempValue(value);
+          setIsEditing(true);
+        }}
+        ml="2"
+      />
+    );
+  };
+
+  useEffect(() => {
+    if (isEditing && ref.current) {
+      (ref.current as HTMLInputElement | HTMLTextAreaElement).focus();
+    }
+  }, [isEditing]);
+
+  return (
+    <Box {...boxProps}>
+      {isEditing ? (
+        isTextArea ? (
+          <FormControl pb={5} isInvalid={isInvalid && isEditing}>
+            <Textarea
+              ref={ref as React.RefObject<HTMLTextAreaElement>}
+              value={tempValue}
+              placeholder={placeholder}
+              onChange={(e) => setTempValue(e.target.value)}
+              onBlur={() => {
+                setIsInvalid(checkError(tempValue) !== 0);
+                onBlur();
+              }}
+              onFocus={() => {
+                setIsInvalid(false);
+                onFocus();
+              }}
+              {...(inputProps as TextareaProps)}
+            />
+            <HStack>
+              <FormErrorMessage>
+                {localeKey &&
+                  (isInvalid && isEditing
+                    ? t(`${localeKey}.error-${checkError(tempValue)}`)
+                    : "")}
+              </FormErrorMessage>
+              <Box mt="2" ml="auto">
+                {EditButtons()}
+              </Box>
+            </HStack>
+          </FormControl>
+        ) : (
+          <FormControl isInvalid={isInvalid && isEditing}>
+            <HStack>
+              <Input
+                ref={ref as React.RefObject<HTMLInputElement>}
+                value={tempValue}
+                placeholder={placeholder}
+                onChange={(e) => setTempValue(e.target.value)}
+                onBlur={() => {
+                  setIsInvalid(checkError(tempValue) !== 0);
+                  onBlur();
+                }}
+                onFocus={() => {
+                  setIsInvalid(false);
+                  onFocus();
+                }}
+                {...(inputProps as InputProps)}
+              />
+              {EditButtons()}
+            </HStack>
+            <FormErrorMessage>
+              {localeKey &&
+                (isInvalid && isEditing
+                  ? t(`${localeKey}.error-${checkError(tempValue)}`)
+                  : "")}
+            </FormErrorMessage>
+          </FormControl>
+        )
+      ) : isTextArea ? (
+        <Text
+          w="100%"
+          wordBreak="break-all"
+          whiteSpace="pre-wrap"
+          {...textProps}
+        >
+          {value}
+          {EditButtons()}
+        </Text>
+      ) : (
+        <HStack spacing={0}>
+          <Text w="100%" {...textProps}>
+            {value}
+          </Text>
+          {EditButtons()}
+        </HStack>
+      )}
+    </Box>
+  );
+};
+
+export default Editable;
