d89e00a04f3de46a660aeefb519c906924bc323b
⁫Rekawiia Lin <Linhk1606@outlook.com>
2025-01-26 18:17:01 +0800

    refactor(frontend): replace VirtualOptionItem with OptionItem in VirtualOptionItemGroup

--- src/components/common/virtual-option-item.tsx
+++ src/components/common/virtual-option-item.tsx
@@ -1,102 +1,15 @@
-import {
-  Box,
-  BoxProps,
-  Card,
-  Divider,
-  Flex,
-  HStack,
-  Skeleton,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
+import { Box, Card, Divider } from "@chakra-ui/react";
 import React, { useEffect, useRef, useState } from "react";
 import { AutoSizer, List, ListRowProps } from "react-virtualized";
+import { OptionItem, OptionItemProps } from "@/components/common/option-item";
 import { Section, SectionProps } from "@/components/common/section";
 
-export interface VirtualOptionItemProps extends BoxProps {
-  prefixElement?: React.ReactNode;
-  title: string;
-  titleExtra?: React.ReactNode;
-  description?: React.ReactNode;
-  isLoading?: boolean;
-  children: React.ReactNode;
-  childrenOnHover?: boolean;
-}
-
 export interface VirtualOptionItemGroupProps extends SectionProps {
-  items: (VirtualOptionItemProps | React.ReactNode)[];
+  items: (OptionItemProps | React.ReactNode)[];
   withDivider?: boolean;
   itemHeight?: number; // 新增项高度属性
   height?: number; // 新增容器高度属性
 }
-
-export const VirtualOptionItem: React.FC<VirtualOptionItemProps> = ({
-  prefixElement,
-  title,
-  titleExtra,
-  description,
-  isLoading = false,
-  children,
-  childrenOnHover = false,
-  ...boxProps
-}) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  return (
-    <Flex
-      justify="space-between"
-      alignItems="self-start"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      {...boxProps}
-    >
-      <HStack spacing={2.5} overflowY="hidden">
-        {prefixElement && (
-          <Skeleton isLoaded={!isLoading}>{prefixElement}</Skeleton>
-        )}
-        <VStack spacing={0} mr={2} alignItems="start" overflow="hidden">
-          <HStack spacing={2} flexWrap="wrap">
-            <Skeleton isLoaded={!isLoading}>
-              <Text fontSize="xs-sm" className="no-select">
-                {title}
-              </Text>
-            </Skeleton>
-            {titleExtra &&
-              (isLoading ? (
-                <Skeleton isLoaded={!isLoading}>
-                  <Text fontSize="xs-sm">
-                    PLACEHOLDER {/*width holder for skeleton*/}
-                  </Text>
-                </Skeleton>
-              ) : (
-                titleExtra
-              ))}
-          </HStack>
-          {description &&
-            (typeof description === "string" ? (
-              <Skeleton isLoaded={!isLoading}>
-                <Text fontSize="xs" className="secondary-text no-select">
-                  {description}
-                </Text>
-              </Skeleton>
-            ) : (
-              description
-            ))}
-        </VStack>
-      </HStack>
-      {(childrenOnHover ? isHovered : true) &&
-        (typeof children === "string" ? (
-          <Skeleton isLoaded={!isLoading}>
-            <Text fontSize="xs-sm" className="secondary-text">
-              {children}
-            </Text>
-          </Skeleton>
-        ) : (
-          children
-        ))}
-    </Flex>
-  );
-};
 
 export const VirtualOptionItemGroup: React.FC<VirtualOptionItemGroupProps> = ({
   items,
@@ -115,10 +28,10 @@ export const VirtualOptionItemGroup: React.FC<VirtualOptionItemGroupProps> = ({
     }
   }, [items]);
 
-  function isVirtualOptionItemProps(item: any): item is VirtualOptionItemProps {
+  function isOptionItemProps(item: any): item is OptionItemProps {
     return (
-      (item as VirtualOptionItemProps)?.title != null &&
-      (item as VirtualOptionItemProps)?.children != null
+      (item as OptionItemProps)?.title != null &&
+      (item as OptionItemProps)?.children != null
     );
   }
 
@@ -126,15 +39,15 @@ export const VirtualOptionItemGroup: React.FC<VirtualOptionItemGroupProps> = ({
     const item = items[index];
     return (
       <div style={style}>
-        {isVirtualOptionItemProps(item) ? (
-          <VirtualOptionItem
+        {isOptionItemProps(item) ? (
+          <OptionItem
             title={item.title}
             description={item.description}
             titleExtra={item.titleExtra}
             prefixElement={item.prefixElement}
           >
             {item.children}
-          </VirtualOptionItem>
+          </OptionItem>
         ) : (
           item
         )}
