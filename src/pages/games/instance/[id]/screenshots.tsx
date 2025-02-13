d5832a208ee1f29785183fa8ae62b7ff1ea0bd47
Qihao Sun <sun060319@sjtu.edu.cn>
2025-02-13 11:10:55 +0800

    feat(frontend): change icon button in screenshots-page (#126)

--- src/pages/games/instance/[id]/screenshots.tsx
+++ src/pages/games/instance/[id]/screenshots.tsx
@@ -1,45 +1,62 @@
-import { Image } from "@chakra-ui/react";
+import { IconButton, Image, Tooltip } from "@chakra-ui/react";
 import { open } from "@tauri-apps/plugin-shell";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { LuEllipsis } from "react-icons/lu";
 import { CommonIconButton } from "@/components/common/common-icon-button";
 import { Section } from "@/components/common/section";
 import { WrapCardGroup } from "@/components/common/wrap-card";
+import { Screenshot } from "@/models/game-instance";
 import { mockScreenshots } from "@/models/mock/game-instance";
 
 const InstanceScreenshotsPage: React.FC = () => {
   const { t } = useTranslation();
+
+  const ScreenshotsCard = ({ screenshot }: { screenshot: Screenshot }) => {
+    const [isHovered, setIsHovered] = useState(false);
+    return (
+      <div
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <Image
+          src={screenshot.imgSrc}
+          alt={screenshot.fileName}
+          objectFit="cover"
+          w="100%"
+          h="100%"
+          position="relative"
+          borderRadius="md"
+        />
+        {isHovered && (
+          <Tooltip
+            label={t("InstanceScreenshotsPage.button.more")}
+            placement="auto"
+          >
+            <IconButton
+              icon={<LuEllipsis />}
+              aria-label="more"
+              colorScheme="blackAlpha"
+              variant="solid"
+              size="xs"
+              position="absolute"
+              top={1}
+              right={1}
+              onClick={() => {}}
+            />
+          </Tooltip>
+        )}
+      </div>
+    );
+  };
 
   return (
     <Section>
       <WrapCardGroup
         cardAspectRatio={16 / 9}
         items={mockScreenshots.map((screenshot) => ({
-          cardContent: (
-            <>
-              <Image
-                src={screenshot.imgSrc}
-                alt={screenshot.fileName}
-                objectFit="cover"
-                w="100%"
-                h="100%"
-                position="relative"
-                borderRadius="md"
-              />
-              <CommonIconButton
-                icon="open"
-                colorScheme="blackAlpha"
-                variant="solid"
-                size="xs"
-                tooltipPlacement="auto"
-                position="absolute"
-                top={1}
-                right={1}
-                onClick={() => {
-                  open(screenshot.filePath);
-                }}
-              />
-            </>
-          ),
+          cardContent: <ScreenshotsCard screenshot={screenshot} />,
           p: 0,
         }))}
       />
