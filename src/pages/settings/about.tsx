0d456e3eccb9fb128a42252861a401fb76c7aa0b
UNIkeEN <94227543+UNIkeEN@users.noreply.github.com>
2025-02-22 18:13:28 +0800

    feat(design): add new logo! 🎉
    Co-authored-by: Neuteria <90081198+Neuteria@users.noreply.github.com>
    Co-authored-by: Teruteru <1357310795@qq.com>

--- src/pages/settings/about.tsx
+++ src/pages/settings/about.tsx
@@ -5,7 +5,7 @@ import {
   OptionItemGroup,
   OptionItemGroupProps,
 } from "@/components/common/option-item";
-import { TitleFull } from "@/components/logo-title";
+import { TitleFullWithLogo } from "@/components/logo-title";
 import { useLauncherConfig } from "@/contexts/config";
 import { CoreContributorsList } from "./contributors";
 
@@ -17,7 +17,7 @@ const AboutSettingsPage = () => {
     {
       title: t("AboutSettingsPage.about.title"),
       items: [
-        <TitleFull my={1} key={0} />,
+        <TitleFullWithLogo key={0} />,
         {
           title: t("AboutSettingsPage.about.settings.version.title"),
           children: config.version,
