6e5eee7d192f2d1272c3da5f4226f82201e07e07
Yujie Sun <94227543+UNIkeEN@users.noreply.github.com>
2025-02-23 15:07:42 +0800

    fix(backend): fix mapped locale in macOS (#187)

--- src-tauri/src/utils/sys_info.rs
+++ src-tauri/src/utils/sys_info.rs
@@ -14,32 +14,36 @@ pub async fn send_statistics(version: String, os: String) {
 }
 
 pub fn get_mapped_locale() -> String {
-  let locale = locale().unwrap();
   // only apple can do 🌈🧑🏻‍🍳👐🏻
   // The return value of tauri_plugin_os::locale() on macOS(e.g. zh-Hans-CN) differs from that on Windows and Linux(e.g. zh-CN).
+  let locale = locale().unwrap();
+  let matched_locale;
 
-  let language_map = [
-    (
-      "zh-Hans",
-      vec!["zh-CN", "zh-Hans-CN", "wuu-Hans-CN", "yue-Hans-CN"],
-    ),
-    (
-      "zh-Hant",
-      vec![
-        "zh-TW",
-        "zh-HK",
-        "zh-MO",
-        "zh-Hant-TW",
-        "zh-Hant-HK",
-        "zh-Hant-MO",
-        "yue-Hant-CN",
-      ],
-    ),
-  ];
+  #[cfg(target_os = "macos")]
+  {
+    let language_map = [
+      ("zh-Hans", vec!["zh-Hans", "wuu-Hans", "yue-Hans"]),
+      ("zh-Hant", vec!["zh-Hant", "yue-Hant"]),
+    ];
 
-  language_map
-    .iter()
-    .find(|(_, locales)| locales.contains(&locale.as_str()))
-    .map(|(mapped, _)| mapped.to_string())
-    .unwrap_or_else(|| "en".to_string()) // fallback to "en"
+    matched_locale = language_map
+      .iter()
+      .find(|(_, locales)| locales.iter().any(|l| locale.starts_with(l)))
+      .map(|(mapped, _)| mapped.to_string());
+  }
+
+  #[cfg(not(target_os = "macos"))]
+  {
+    let language_map = [
+      ("zh-Hans", vec!["zh-CN"]),
+      ("zh-Hant", vec!["zh-TW", "zh-HK", "zh-MO"]),
+    ];
+
+    matched_locale = language_map
+      .iter()
+      .find(|(_, locales)| locales.contains(&locale.as_str()))
+      .map(|(mapped, _)| mapped.to_string());
+  }
+
+  matched_locale.unwrap_or_else(|| "en".to_string()) // fallback to "en"
 }
