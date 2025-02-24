c7ea200c64a4dea6d7bfa93dfbdc073fedd3cc64
SundayChen <824987073@qq.com>
2025-02-24 23:51:05 +0800

    fix(backend): rename file and dir to avoid overwriting when moving/copying

--- src-tauri/src/instance/commands.rs
+++ src-tauri/src/instance/commands.rs
@@ -1,6 +1,6 @@
 use super::{
   super::utils::{
-    fs::copy_whole_dir,
+    fs::{copy_whole_dir, generate_unique_filename},
     nbt::load_nbt,
     path::{get_files_with_regex, get_subdirectories},
   },
@@ -74,7 +74,7 @@ pub fn copy_across_instances(
         None => return Err(InstanceError::InstanceNotFoundByID.into()),
       };
 
-      let dest_path = Path::new(&tgt_path).join(&filename);
+      let dest_path = generate_unique_filename(&tgt_path, &filename);
       fs::copy(&src_file_path, &dest_path).map_err(|_| InstanceError::FileCopyFailed)?;
     }
   } else if src_path.is_dir() {
@@ -84,7 +84,7 @@ pub fn copy_across_instances(
         None => return Err(InstanceError::InstanceNotFoundByID.into()),
       };
 
-      let dest_path = Path::new(&tgt_path).join(src_path.file_name().unwrap());
+      let dest_path = generate_unique_filename(&tgt_path, src_path.file_name().unwrap());
       copy_whole_dir(src_path, &dest_path).map_err(|_| InstanceError::FileCopyFailed)?;
     }
   } else {
@@ -111,7 +111,7 @@ pub fn move_across_instances(
     None => return Err(InstanceError::InvalidSourcePath.into()),
   };
 
-  let dest_path = Path::new(&tgt_path).join(&filename);
+  let dest_path = generate_unique_filename(&tgt_path, &filename);
   fs::rename(&src_file_path, &dest_path).map_err(|_| InstanceError::FileMoveFailed)?;
   Ok(())
 }
--- src-tauri/src/utils/fs.rs
+++ src-tauri/src/utils/fs.rs
@@ -1,5 +1,6 @@
+use std::ffi::OsStr;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 pub fn copy_whole_dir(src: &Path, dst: &Path) -> std::io::Result<()> {
   if !dst.exists() {
@@ -18,4 +19,44 @@ pub fn copy_whole_dir(src: &Path, dst: &Path) -> std::io::Result<()> {
     }
   }
   Ok(())
+}
+
+pub fn generate_unique_filename(base_path: &Path, filename: &OsStr) -> PathBuf {
+  let (name, extension) = split_filename(filename);
+  let mut dest_path = base_path.join(filename);
+  let mut counter = 1;
+
+  while dest_path.exists() {
+    let new_filename = format!(
+      "{} copy{}",
+      name,
+      if counter > 1 {
+        format!(" {}", counter)
+      } else {
+        String::new()
+      }
+    );
+
+    let new_filename = if !extension.is_empty() {
+      format!("{}.{}", new_filename, extension)
+    } else {
+      new_filename
+    };
+
+    dest_path = base_path.join(new_filename);
+    counter += 1;
+  }
+
+  dest_path
+}
+
+pub fn split_filename(filename: &OsStr) -> (String, String) {
+  let filename_str = filename.to_string_lossy();
+  if let Some(dot_index) = filename_str.rfind('.') {
+    let name = filename_str[..dot_index].to_string();
+    let extension = filename_str[dot_index + 1..].to_string();
+    (name, extension)
+  } else {
+    (filename_str.to_string(), String::new())
+  }
 }
