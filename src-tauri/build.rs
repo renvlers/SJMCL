9e99e29a2a0dc1d81ce642e7f794e3f2e09dbb4b
UNIkeEN <94227543+UNIkeEN@users.noreply.github.com>
2025-03-21 00:09:40 +0800

    feat, chore(backend): update build.rs to add secret consts

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,32 @@
+use dotenvy::{from_filename, dotenv_override};
+use std::{env, fs, path::Path};
+
 fn main() {
+  // Load env variables from ".env" file, if not exists, use ".env.template" to set default value.
+  from_filename(".env.template").ok();
+  dotenv_override().ok(); 
+
+  let out_dir = env::var("OUT_DIR").unwrap();
+  let dest_path = Path::new(&out_dir).join("secrets.rs");
+  let _ = fs::remove_file(&dest_path);
+
+  // Iterate over all env variables and write those starting with "SJMCL_" into "secrets.rs" as pub const
+  // To use secrets, try "include!(concat!(env!("OUT_DIR"), "/secrets.rs"));"
+  let mut extra_envs = String::new();
+  for (key, value) in env::vars() {
+    if key.starts_with("SJMCL_") {
+      extra_envs.push_str(&format!(
+        "pub const {}: &str = \"{}\";\n",
+        key.replace("-", "_"),
+        value
+      ));
+    }
+  }
+  fs::write(&dest_path, extra_envs).expect("Failed to write secrets.rs");
+
+  // Notify Cargo to auto re-run the build script if .env changes
+  println!("cargo::rerun-if-changed=.env");
+  println!("cargo::rerun-if-changed=.env.template");
+
   tauri_build::build()
 }
