2ead2c484733beddaa56af75d0a5bc9bb651ff60
pangbo13 <51732678+pangbo13@users.noreply.github.com>
2025-03-25 18:53:20 +0800

    feat(ci/cd): add secrets when building

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -2,11 +2,13 @@ use dotenvy::{from_filename, dotenv_override};
 use std::{env, fs, path::Path};
 
 fn main() {
-  // Load env variables from ".env" file, if not exists, use ".env.template" to set default value.
-  from_filename(".env.template").ok();
-  dotenv_override().ok(); 
+  if !std::env::var("GITHUB_ACTIONS").is_ok() {
+    // Load env variables from ".env" file, if not exists, use ".env.template" to set default value.
+    from_filename(".env.template").ok();
+    dotenv_override().ok(); 
+  }
 
-  let out_dir = env::var("OUT_DIR").unwrap();
+  let out_dir = env::var("OUT_DIR").unwrap_or_else(|_| "".to_string());
   let dest_path = Path::new(&out_dir).join("secrets.rs");
   let _ = fs::remove_file(&dest_path);
 
